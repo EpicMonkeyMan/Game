33fc5586d0c283f41cd4dde74e0b6f71e9af3f27
Sondrake <smt1998x@hotmail.com>
2017-03-28 20:14:10 +0200

    Added textures

--- shaders/sprite.vert
+++ shaders/sprite.vert
@@ -1,11 +1,15 @@
 #version 140
 
 in vec2 position;
+in vec2 tex_coords;
+
+out vec2 v_tex_coords;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
-    gl_Position = projection * view * model * vec4(position, 1.0f);
+    gl_Position = projection * view * model * vec4(position, 0.0f, 1.0f);
+    v_tex_coords = tex_coords;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
 #[macro_use]
 extern crate glium;
+extern crate image;
 
 fn main() {
     use glium::{DisplayBuild, Surface};
+    use std::io::Cursor;
 
     let window = glium::glutin::WindowBuilder::new()
         .with_vsync()
+        .with_multisampling(8u16)
         .with_title("David er en pen person")
         .with_fullscreen(glium::glutin::get_primary_monitor())
         .build_glium()
         .unwrap();
 
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
-    }
+    let image = image::load(Cursor::new(&include_bytes!("../textures/david2.png")[..]), image::PNG).unwrap().to_rgba();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(), image_dimensions);
+    let texture = glium::texture::CompressedSrgbTexture2d::new(&window, image).unwrap();
 
-    implement_vertex!(Vertex, position);
+    let vertex_buffer = {
+        #[derive(Copy, Clone)]
+        struct Vertex {
+            position: [f32; 2],
+            tex_coords: [f32; 2],
+        }
 
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.5,  -0.5] };
-    let vertex3 = Vertex { position: [ -0.5, 0.5] };
-    let vertex4 = Vertex { position: [ 0.5, 0.5] };
-    let shape = vec![vertex1, vertex2, vertex3, vertex4];
+        implement_vertex!(Vertex, position, tex_coords);
 
-    let vertex_buffer = glium::VertexBuffer::new(&window, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
+        glium::VertexBuffer::new(&window, 
+            &[
+                Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] },
+                Vertex { position: [-0.5,  0.5], tex_coords: [0.0, 1.0] },
+                Vertex { position: [ 0.5,  0.5], tex_coords: [1.0, 1.0] },
+                Vertex { position: [ 0.5, -0.5], tex_coords: [1.0, 0.0] }
+            ]
+        ).unwrap()
+    };
+
+    let index_buffer = glium::IndexBuffer::new(&window, glium::index::PrimitiveType::TriangleStrip, &[1 as u16, 2, 0, 3]).unwrap();
 
     let mut vshader_src = String::new();
     let mut fshader_src = String::new();
@@ -43,7 +56,38 @@ fn main() {
 
     let program = glium::Program::from_source(&window, vshader_src.as_str(), fshader_src.as_str(), None).unwrap();
 
+    let model_matrix = [
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0f32]
+    ];
+    let view_matrix = [
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0f32]
+    ];
+    let projection_matrix = [
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0f32]
+    ];
+
     'gameloop: loop {
+        let uniforms = uniform! {
+            model: model_matrix,
+            view: view_matrix,
+            projection: projection_matrix,
+            tex: &texture
+        };
+
+        let mut target = window.draw();
+        target.clear_color(0.2, 0.2, 0.2, 1.0); 
+        target.draw(&vertex_buffer, &index_buffer, &program, &uniforms, &Default::default()).unwrap();
+        target.finish().unwrap();
+
         for e in window.poll_events() {
             use glium::glutin::Event;
 
@@ -52,11 +96,5 @@ fn main() {
                 _ => (),
             }
         }
-
-        let mut target = window.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
-        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
-                    &Default::default()).unwrap();
-        target.finish().unwrap();
     }
 }
