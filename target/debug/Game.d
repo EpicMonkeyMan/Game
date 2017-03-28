d81666b0107af03e55729e45fcd57d78ad99462b
Dennis Kristiansen <dennkris@live.no>
2017-03-28 12:47:35 +0200

    Remove ignored files

--- target/debug/Game.d
+++
@@ -1,1 +0,0 @@
-c:\Users\sondremt\Desktop\Sondre\Rust\ Projects\Game\target\debug\Game.exe: c:\Users\sondremt\Desktop\Sondre\Rust\ Projects\Game\src\main.rs
--- target/debug/deps/David-23ef1009407173d3.d
+++
@@ -1,5 +0,0 @@
-c:\Users\sondremt\Desktop\Sondre\Rust Projects\Game\target\debug\deps\David-23ef1009407173d3.exe: src\main.rs
-
-c:\Users\sondremt\Desktop\Sondre\Rust Projects\Game\target\debug\deps\David-23ef1009407173d3.d: src\main.rs
-
-src\main.rs:
